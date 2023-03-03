3129095d46c2d8597dac19eb6bfc9289a22ca67e
Liquid Monks <info@liquidmonks.com>
2023-03-03 14:22:27 -0600

    models/Thought.js defines a Mongoose schema for a thought model and exports it as a model for use in other parts of this application.

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+// Import required modules
+const { Schema, model } = require("mongoose");
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      // Validate the length of the thought text
+      validate: {
+        validator: (length) => length > 0 && length <= 280,
+        message: "Thoughts can only be between 1 and 280 characters long!",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    // Adds the reactions schema to the thought schema
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      // Includes getters when converting the object to JSON
+      getters: true,
+      // Includes virtual properties when converting the object to JSON
+      virtuals: true,
+    },
+  }
+);
+
+// Defines the virtual property 'reactionCount'
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// Creates the Thought model
+const Thought = model("Thought", ThoughtSchema);
+
+// Export the Thought model
+module.exports = Thought;

a34e7bd20f757b59b92664ff7a7ea08a63d15f2f
Liquid Monks <info@liquidmonks.com>
2023-03-03 15:48:14 -0600

    models/index.js includes developer comments.

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
+// Imports the Thought and User models
 const User = require("./User");
 const Thought = require("./Though");
 
+// Exports the Thought and User models as properties of an object
 module.exports = { User, Thought };

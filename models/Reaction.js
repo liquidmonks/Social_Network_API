49251f5b771baa16d51dd9eea87d034c868caaa4
Liquid Monks <info@liquidmonks.com>
2023-03-03 15:37:29 -0600

    models/Reaction.js defines the Mongoose schema for a reaction that can be ued to model a reaction to a thought or reaction.

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+// Import required models
+const { Schema, Types } = require("mongoose");
+
+// Defines the ReactionSchema
+const ReactionSchema = new Schema({
+  reactionID: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    validate: {
+      // The validator function checks the length of the reactionBody field
+      validator: ({ length }) => length <= 280,
+      // The message to display if the validation fails
+      message: "Reactions cannot be more than 280 characters long!",
+    },
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // The getter function formats the createdAt field using the dateFormat utility function
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+});
+
+// Exports the ReactionSchema
+module.exports = ReactionSchema;

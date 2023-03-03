e4f120ce1bf1a2474048f33dddbf7f76fe6efe45
Liquid Monks <info@liquidmonks.com>
2023-03-03 15:43:42 -0600

    models/Thought.js imports the Reaction model to present user reactions in the Thought schema.

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,7 @@
 // Import required modules
 const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
+const reactionSchema = require("./Reaction");
 
 const ThoughtSchema = new Schema(
   {

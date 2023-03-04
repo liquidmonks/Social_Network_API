3489cbf790599414d420a8307e3cb4cc32744ca5
Liquid Monks <info@liquidmonks.com>
2023-03-04 08:58:58 -0600

    controllers/though-controller.js includes a getUsers codeblock that retrieves all user documments fromt he database using the 'User' model, and sends the data as a JSON response to the client.

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,7 +2,17 @@
 const { Thought, User } = require("../models");
 
 // Defines an object called thoughtController with several methods
-const thoughtController = {};
+const thoughtController = {
+  async getUsers(req, res) {
+    try {
+      const userData = await User.findOne({});
+      res.json(userData);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  },
+};
 
 // Exports the thoughtController
 module.exports = thoughtController;

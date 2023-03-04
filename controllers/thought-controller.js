37d345fcc48d05cb4c12067276327f3666feb8b9
Liquid Monks <info@liquidmonks.com>
2023-03-04 08:22:38 -0600

    controllers/thought-controller.js defines a thoughtController code block that will contain the methods to perform CRUD operations on the Thought model in the database.

---
+++ controllers/thought-controller.js
@@ -0,0 +1,8 @@
+// Imports the Thought and User models from the '../models' module
+const { Thought, User } = require("../models");
+
+// Defines an object called thoughtController with several methods
+const thoughtController = {};
+
+// Exports the thoughtController
+module.exports = thoughtController;

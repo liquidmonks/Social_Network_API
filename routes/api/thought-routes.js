a59c21d59354f826f25d7d19f74601f3227d69cd
Liquid Monks <info@liquidmonks.com>
2023-03-05 08:53:30 -0600

    routes/api/thought-routes defines routes for an API that handles user thoughts and their reactions.

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,22 @@
+// Imports necessary modules and controllers
+const express = require("express");
+const router = express.Router();
+const thoughtController = require("../../controllers/thought-controller");
+
+// Defines route to get all thoughts
+router.route("/thoughts").get(thoughtController.getThoughts);
+
+// Route to add a new thought for a user
+router.route("/thoughts/:userID").post(thoughtController.addThought);
+
+// Route to get a specific thought by ID, update a thought, and delete a thought
+router.route("/thoughts/:thoughtId").get(thoughtController.getThoughtByID).put(thoughtController.updateThought).delete(thoughtController.deleteThought);
+
+// Route to add a reaction to a thought
+router.route("/thoughts/:thoughtId/reactions").post(thoughtController.addReaction);
+
+// Route to delete a reaction from a thought
+router.route("/thoughts/:thoughtId/reactions/:reactionId").delete(thoughtController.deleteReaction);
+
+// Exports router module
+module.exports = router;
